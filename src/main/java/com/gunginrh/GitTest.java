3fb4af80face373b4f79100b4f7096f01f6809a4
hhw <hhw519849102@gmail.com>
2022-05-06 16:43:40 +0800

    second test

--- src/main/java/com/gunginrh/GitTest.java
+++ src/main/java/com/gunginrh/GitTest.java
@@ -3,5 +3,8 @@ package com.gunginrh;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello github!");
+        System.out.println("hello github!");
+        System.out.println("hello github!");
     }
 }
