02a671ee094fcd9b902ba752a62c3295fc46da2e
hhw <hhw519849102@gmail.com>
2022-05-07 11:48:01 +0800

    github push test

--- src/main/java/com/gunginrh/GitTest.java
+++ src/main/java/com/gunginrh/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("hxxxx");
         System.out.println("1111");
+        System.out.println("1111");
     }
 }
